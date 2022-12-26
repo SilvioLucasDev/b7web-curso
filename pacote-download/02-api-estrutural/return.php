8bf02e62be648a6d2757950cc505b7f9f31a25e3
Silvo Lucas <silviolucas_santos@hotmail.com>
2022-12-26 02:46:56 -0300

    Criação do endpoint que altera um nota no db

--- pacote-download/02-api-estrutural/return.php
+++ pacote-download/02-api-estrutural/return.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json");
 
 echo json_encode($array, JSON_UNESCAPED_UNICODE);
