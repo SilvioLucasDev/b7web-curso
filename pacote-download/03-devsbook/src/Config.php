54b54c6ebb33e9e6e6d00a02f845b3472e767208
Silvio Lucas <silviolucas_santos@hotmail.com>
2022-12-27 15:07:14 -0300

    Criado o processo para validação de login

--- pacote-download/03-devsbook/src/Config.php
+++ pacote-download/03-devsbook/src/Config.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace src;
 
-class Config {
+class Config
+{
     const BASE_DIR = '/b7web-curso/pacote-download/03-devsbook/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
     const DB_DATABASE = 'devsbook';
-    CONST DB_USER = 'root';
+    const DB_USER = 'root';
     const DB_PASS = '';
 
     const ERROR_CONTROLLER = 'ErrorController';
