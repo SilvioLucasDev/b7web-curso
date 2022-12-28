b32d9f681b14e1e047368f4f77742166e3e1c182
Silvio Lucas <silviolucas_santos@hotmail.com>
2022-12-27 22:10:52 -0300

    Criado página de login e a validação

--- pacote-download/03-devsbook/src/routes.php
+++ pacote-download/03-devsbook/src/routes.php
@@ -5,5 +5,8 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/login', 'LoginController@signin');
+$router->post('/login', 'LoginController@signinAction');
+$router->get('/cadastro', 'LoginController@sigup');
 $router->get('/cadastro', 'LoginController@sigup');
